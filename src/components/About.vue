363220e2afd04163b689b7673efea5654aca35e0
Damian <damianladiani@gmail.com>
2021-05-24 20:18:38 -0300

    Hago parte de about y why wadapay

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,4 @@
 <template>
-    
-    <!-- START SECTION ABOUT US -->
     <section id="about" class="imagen-banner">
         <div class="container">
             <div class="row align-items-center">
@@ -14,15 +12,13 @@
                 </div>
                 <div class="col-lg-6 col-md-12 col-sm-12 text_md_center">
                     <div class="title_cyan_dark">
-                    <h4 class="animation" data-animation="fadeInUp" data-animation-delay="0.4s">Sobre Wadapay</h4>
-                    <p class="animation" data-animation="fadeInUp" data-animation-delay="0.6s">Creada para funcionar como medio de pago de Wadaboo y está integrada de manera automática a las ventas individuales de las empresas.</p>
-                    <p class="animation" data-animation="fadeInUp" data-animation-delay="1s">De esta manera, Wadaboo tiene una moneda nativa que puede ser usada para que las empresas paguen, cobren, hagan transferencias internacionales o saquen préstamos.</p>
-                    <a href="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xEE1e22DB567897bB401311B79030344602EABE47&inputCurrency=BNB" target="_blank" class="btn btn-default btn-radius">¿Dónde comprar? <i class="ion-ios-arrow-thin-right"></i></a>
+                    <h4 class="animation" data-animation="fadeInUp" data-animation-delay="0.4s">{{ $t("about.title-about") }}</h4>
+                    <p class="animation" data-animation="fadeInUp" data-animation-delay="0.6s">{{ $t("about.paragraph1") }}</p>
+                    <p class="animation" data-animation="fadeInUp" data-animation-delay="1s">{{ $t("about.paragraph2") }}</p>
+                    <a href="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xEE1e22DB567897bB401311B79030344602EABE47&inputCurrency=BNB" target="_blank" class="btn btn-default btn-radius">{{ $t("about.where-buy") }} <i class="ion-ios-arrow-thin-right"></i></a>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <!-- END SECTION ABOUT US --> 
-
 </template>
