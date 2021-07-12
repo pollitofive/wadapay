2701d0eb244fdc21fe543c8cec2be5976eaa203a
Damian <damianladiani@gmail.com>
2021-07-12 18:54:27 -0300

    Subo cambios minimos

--- src/components/StartBanner.vue
+++ src/components/StartBanner.vue
@@ -19,7 +19,7 @@
                     <div class="banner_inner token_gradiant position-relative">
                         <div class="tk_countdown transparent_bg token_bg token_circle text-center animation" data-animation="fadeIn" data-animation-delay="1.1s">
                             <div class="banner_text tk_counter_inner">
-                                <div class="tk_countdown_time p-0 transparent_bg box_shadow_none animation" data-animation="fadeInUp" data-animation-delay="1.2s" data-time="2021/06/15 12:00:00"></div>
+                                <div class="tk_countdown_time p-0 transparent_bg box_shadow_none animation" data-animation="fadeInUp" data-animation-delay="1.2s" data-time="2021/07/26 12:00:00"></div>
                                 <a href="https://forms.gle/Zjrk3VvePqQi7gkr5" class="btn btn-default btn-radius animation" data-animation="fadeInUp" data-animation-delay="1.40s" target="_blank">{{ $t("start-banner.buy-tokens")}}<i class="ion-ios-arrow-thin-right"></i></a>
                             </div>
                         </div>
@@ -29,14 +29,16 @@
                             <div class="row">
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 pl-2 pr-2">
                                     <div class="pr_box">
-                                        <h6 class="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.2s" style="animation-delay: 0.2s; opacity: 1;">{{ $t("start-banner.total-created")}}:</h6>
-                                        <p class="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.4s" style="animation-delay: 0.4s; opacity: 1;">200.000.000.000 Wapay</p>
+                                        <h6 class="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.2s" style="animation-delay: 0.2s; opacity: 1;">{{ $t("start-banner.begin")}}:</h6>
+                                        <p class="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.2s" style="animation-delay: 0.2s; opacity: 1;">{{ $t("start-banner.begin-date")}}</p>
+                                        <p class="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.4s" style="animation-delay: 0.4s; opacity: 1;">{{ $t("start-banner.begin-time")}}</p>
                                     </div>
                                 </div>
                                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 pl-2 pr-2">
                                     <div class="pr_box">
-                                        <h6 class="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.6s" style="animation-delay: 0.6s; opacity: 1;">{{ $t("start-banner.start-sell")}}:</h6>
-                                        <p class="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.8s" style="animation-delay: 0.8s; opacity: 1;">{{ $t("start-banner.date-start-sell") }} <br>{{ $t("start-banner.hour-start-sell") }}</p>
+                                        <h6 class="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.6s" style="animation-delay: 0.6s; opacity: 1;">{{ $t("start-banner.end")}}:</h6>
+                                        <p class="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.2s" style="animation-delay: 0.2s; opacity: 1;">{{ $t("start-banner.end-date")}}</p>
+                                        <p class="animation animated fadeInUp" data-animation="fadeInUp" data-animation-delay="0.8s" style="animation-delay: 0.8s; opacity: 1;">{{ $t("start-banner.end-time") }}</p>
                                     </div>
                                 </div>
                             </div>
